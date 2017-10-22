effbbf757cd6d195df9a6d1b2c9e3fef647c1944
Mastavps <samreysteven91@gmail.com>
2017-10-22 14:44:19 +0800

    Create motd.sh

---
+++ motd.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+apt-get update 
+apt-get install ca-certificates -y
+apt-get install vnstat -y
+apt-get install wget -y
+
+
+cd /usr/bin/
+wget https://raw.githubusercontent.com/samreysteven/newmenu/master/custom_motd
+chmod +x custom_motd
+
+echo 'custom_motd' >> /etc/profile
